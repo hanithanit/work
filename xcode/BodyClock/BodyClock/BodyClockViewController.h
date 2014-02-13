9aad4e525ca5e0792752251a685b011ff0ff165c
hanithanit <teamhanit@gmail.com>
2014-02-13 20:01:19 +0900

    BodyClock

---
+++ xcode/BodyClock/BodyClock/BodyClockViewController.h
@@ -0,0 +1,13 @@
+//
+//  BodyClockViewController.h
+//  BodyClock
+//
+//  Created by kentaru on 2014/02/13.
+//  Copyright (c) 2014年 hogehoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BodyClockViewController : UIViewController
+
+@end
---
+++ xcode/BodyClock/BodyClock/BodyClockViewController.m
@@ -0,0 +1,29 @@
+//
+//  BodyClockViewController.m
+//  BodyClock
+//
+//  Created by kentaru on 2014/02/13.
+//  Copyright (c) 2014年 hogehoge. All rights reserved.
+//
+
+#import "BodyClockViewController.h"
+
+@interface BodyClockViewController ()
+
+@end
+
+@implementation BodyClockViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
