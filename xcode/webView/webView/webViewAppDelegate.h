ee2fad6acf98eb692d38152f6b0100f150640d82
hanithanit <teamhanit@gmail.com>
2014-04-19 19:19:02 +0900

    first webView

---
+++ xcode/webView/webView/webViewAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  webViewAppDelegate.h
+//  webView
+//
+//  Created by kentaru on 2014/04/19.
+//  Copyright (c) 2014年 hogehoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface webViewAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ xcode/webView/webView/webViewViewController.h
@@ -0,0 +1,14 @@
+//
+//  webViewViewController.h
+//  webView
+//
+//  Created by kentaru on 2014/04/19.
+//  Copyright (c) 2014年 hogehoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface webViewViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
