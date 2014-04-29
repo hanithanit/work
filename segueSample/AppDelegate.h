a2ba29c67cdecea49cc2e4667d7cb0da0decee56
hanithanit <teamhanit@gmail.com>
2014-04-29 19:17:09 +0900

    add secondwebView

---
+++ segueSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  segueSample
+//
+//  Created by kentaru on 2014/04/29.
+//  Copyright (c) 2014年 hogehoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
